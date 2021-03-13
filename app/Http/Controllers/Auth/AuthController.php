0cc961916ee4e63c874bc6057f5f8afe05fac271
swatantra <swatantra0723@gmail.com>
2021-03-13 19:18:33 +0545

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use GuzzleHttp;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        $http = new GuzzleHttp\Client();
+        try {
+            $response = $http->post(config('services.passport.login_endpoint'), [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => config('services.passport.client_id'),
+                    'client_secret' => config('services.passport.client_secret'),
+                    'username' => request('username'),
+                    'password' => request('password'),
+                ]
+            ]);
+            return $response->getBody();
+        } catch (GuzzleHttp\Exception\BadResponseException $e) {
+            if ($e->getCode() === 400)
+                return response()->json('Invalid Request. Please enter username and password.', $e->getCode());
+            elseif ($e->getCode() === 401)
+                return response()->json('Invalid credentials. Please try again.', $e->getCode());
+
+
+            return response()->json('Something went wrong on the server.', $e->getCode());
+        }
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+        return response()->json('Logged out successfully.');
+    }
+}
